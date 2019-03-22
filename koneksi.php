088c2cde733f0bcb2fb958de8233fe18eac8b633
frankaditya08 <48473335+frankaditya08@users.noreply.github.com>
2019-03-22 16:17:56 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id7996950_aditya2","frankaditya0138","id7996950_tugas2");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
